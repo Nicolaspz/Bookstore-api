e5c3514cae792d5e1770dfa4ddbc4146b634807c
Nicolaspz <nicolaspz14@hotmail.com>
2022-11-21 15:19:45 +0100

    salvando na Base de Dados H2

--- src/main/java/com/nicolas/Booksore/BooksoreApplication.java
+++ src/main/java/com/nicolas/Booksore/BooksoreApplication.java
@@ -1,13 +1,37 @@
 package com.nicolas.Booksore;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.nicolas.Booksore.domain.Categoria;
+import com.nicolas.Booksore.domain.Livro;
+import com.nicolas.Booksore.repositories.CategoriRepository;
+import com.nicolas.Booksore.repositories.LivroRepository;
+
 @SpringBootApplication
-public class BooksoreApplication {
+public class BooksoreApplication implements CommandLineRunner {
+	@Autowired
+	CategoriRepository cate;
+	@Autowired
+	LivroRepository livro;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BooksoreApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+	 Categoria cat1 = new Categoria(null,"Infoemática","Poesia nua e Crua");
+	 Livro l1 = new Livro(null, "Clean Code", "Robert Martin", "Lorem Ipsun", cat1);
+	 cat1.getLivros().addAll(Arrays.asList(l1));
+	 this.cate.saveAll(Arrays.asList(cat1));
+	 this.livro.saveAll(Arrays.asList(l1));
+		
 	}
 
 }
