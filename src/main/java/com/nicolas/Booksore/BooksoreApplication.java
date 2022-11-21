0fe21d1b1e63765f3f908456b2e27049e064456b
Nicolaspz <nicolaspz14@hotmail.com>
2022-11-21 12:26:31 +0100

    criação do projecto

---
+++ src/main/java/com/nicolas/Booksore/BooksoreApplication.java
@@ -0,0 +1,13 @@
+package com.nicolas.Booksore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nicolas/Booksore/BooksoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicolas.Booksore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
